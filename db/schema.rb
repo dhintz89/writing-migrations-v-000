892a0bd0f14397550e6cf6850899ccaec7dd5491
Dan Hintz <dhintz@live.com>
2018-12-24 22:33:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 3) do
 
-# Could not dump table "students" because of following StandardError
-#   Unknown type 'String' for column 'birthdate'
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "grade"
+    t.datetime "birthdate"
+  end
 
 end
